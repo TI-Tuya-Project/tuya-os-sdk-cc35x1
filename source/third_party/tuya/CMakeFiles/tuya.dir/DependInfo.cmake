f06c73f5cbd173464ef45e575b7afaf4ce28b3b2
Samuel Lazareanu <52662032+SamuraiPolix@users.noreply.github.com>
2025-12-30 13:28:14 +0200

    tkl_output

---
+++ source/third_party/tuya/CMakeFiles/tuya.dir/DependInfo.cmake
@@ -0,0 +1,111 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/include/init/src/tkl_init_network.c" "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/CMakeFiles/tuya.dir/ti_config/tkl/tuyaos/tuyaos_adapter/include/init/src/tkl_init_network.c.obj"
+  "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/include/init/src/tkl_init_system.c" "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/CMakeFiles/tuya.dir/ti_config/tkl/tuyaos/tuyaos_adapter/include/init/src/tkl_init_system.c.obj"
+  "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/include/init/src/tkl_init_wifi.c" "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/CMakeFiles/tuya.dir/ti_config/tkl/tuyaos/tuyaos_adapter/include/init/src/tkl_init_wifi.c.obj"
+  "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/src/tkl_flash.c" "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/CMakeFiles/tuya.dir/ti_config/tkl/tuyaos/tuyaos_adapter/src/tkl_flash.c.obj"
+  "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/src/tkl_gpio.c" "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/CMakeFiles/tuya.dir/ti_config/tkl/tuyaos/tuyaos_adapter/src/tkl_gpio.c.obj"
+  "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/src/tkl_memory.c" "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/CMakeFiles/tuya.dir/ti_config/tkl/tuyaos/tuyaos_adapter/src/tkl_memory.c.obj"
+  "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/src/tkl_mutex.c" "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/CMakeFiles/tuya.dir/ti_config/tkl/tuyaos/tuyaos_adapter/src/tkl_mutex.c.obj"
+  "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/src/tkl_network.c" "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/CMakeFiles/tuya.dir/ti_config/tkl/tuyaos/tuyaos_adapter/src/tkl_network.c.obj"
+  "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/src/tkl_output.c" "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/CMakeFiles/tuya.dir/ti_config/tkl/tuyaos/tuyaos_adapter/src/tkl_output.c.obj"
+  "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/src/tkl_semaphore.c" "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/CMakeFiles/tuya.dir/ti_config/tkl/tuyaos/tuyaos_adapter/src/tkl_semaphore.c.obj"
+  "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/src/tkl_system.c" "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/CMakeFiles/tuya.dir/ti_config/tkl/tuyaos/tuyaos_adapter/src/tkl_system.c.obj"
+  "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/src/tkl_thread.c" "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/CMakeFiles/tuya.dir/ti_config/tkl/tuyaos/tuyaos_adapter/src/tkl_thread.c.obj"
+  "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/src/tkl_uart.c" "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/CMakeFiles/tuya.dir/ti_config/tkl/tuyaos/tuyaos_adapter/src/tkl_uart.c.obj"
+  "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/src/tkl_wifi.c" "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/CMakeFiles/tuya.dir/ti_config/tkl/tuyaos/tuyaos_adapter/src/tkl_wifi.c.obj"
+  "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/tuya_src/common/backoffAlgorithm/source/backoff_algorithm.c" "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/CMakeFiles/tuya.dir/tuya_src/common/backoffAlgorithm/source/backoff_algorithm.c.obj"
+  "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/tuya_src/common/utilities/crc32i.c" "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/CMakeFiles/tuya.dir/tuya_src/common/utilities/crc32i.c.obj"
+  "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/tuya_src/common/utilities/uni_random.c" "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/CMakeFiles/tuya.dir/tuya_src/common/utilities/uni_random.c.obj"
+  "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/tuya_src/libcjson/cJSON/cJSON.c" "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/CMakeFiles/tuya.dir/tuya_src/libcjson/cJSON/cJSON.c.obj"
+  "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/tuya_src/tal_network/src/tal_network.c" "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/CMakeFiles/tuya.dir/tuya_src/tal_network/src/tal_network.c.obj"
+  "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/tuya_src/tal_system/src/tal_log.c" "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/CMakeFiles/tuya.dir/tuya_src/tal_system/src/tal_log.c.obj"
+  "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/tuya_src/tal_system/src/tal_sleep.c" "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/CMakeFiles/tuya.dir/tuya_src/tal_system/src/tal_sleep.c.obj"
+  "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/tuya_src/tal_system/src/tal_system.c" "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/CMakeFiles/tuya.dir/tuya_src/tal_system/src/tal_system.c.obj"
+  "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/tuya_src/tal_system/src/tal_thread.c" "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/CMakeFiles/tuya.dir/tuya_src/tal_system/src/tal_thread.c.obj"
+  "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/tuya_src/tal_wifi/src/tal_wifi.c" "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/CMakeFiles/tuya.dir/tuya_src/tal_wifi/src/tal_wifi.c.obj"
+  "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/tuya_src/tuya_cloud_service/cloud/mqtt_service.c" "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/CMakeFiles/tuya.dir/tuya_src/tuya_cloud_service/cloud/mqtt_service.c.obj"
+  "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/tuya_src/tuya_cloud_service/cloud/tuya_iot.c" "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/CMakeFiles/tuya.dir/tuya_src/tuya_cloud_service/cloud/tuya_iot.c.obj"
+  "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/tuya_src/tuya_cloud_service/protocol/tuya_protocol.c" "C:/ti/simplelink_wifi_sdk_tuya/source/third_party/tuya/CMakeFiles/tuya.dir/tuya_src/tuya_cloud_service/protocol/tuya_protocol.c.obj"
+  )
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CC35XX"
+  "DeviceFamily_CC35XX"
+  "TUYA_GW_OPERATING_SYSTEM=1"
+  "_TUYA_TI_PLATFORM_"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "source/third_party/tuya/tuya_src/include/common"
+  "source/third_party/tuya/tuya_src/tal_system/include"
+  "source/third_party/tuya/tuya_src/tal_network/include"
+  "source/third_party/tuya/tuya_src/tal_wifi/include"
+  "source/third_party/tuya/tuya_src/common/include"
+  "source/third_party/tuya/tuya_src/include"
+  "source/third_party/tuya/ti_config"
+  "source/third_party/tuya/tuya_src/tuya_cloud_service/netmgr"
+  "source/third_party/tuya/tuya_src/tuya_cloud_service/cloud"
+  "source/third_party/tuya/tuya_src/tuya_cloud_service/schema"
+  "source/third_party/tuya/tuya_src/tuya_cloud_service/protocol"
+  "source/third_party/tuya/tuya_src/tuya_cloud_service/tls"
+  "source/third_party/tuya/tuya_src/common/utilities"
+  "source/third_party/tuya/tuya_src/libtls/include"
+  "source/third_party/tuya/tuya_src/libtls/port"
+  "source/third_party/tuya/tuya_src/libtls/mbedtls-3.1.0/include"
+  "source/third_party/tuya/third_party/hostap/src/openssl/include"
+  "source/third_party/tuya/tuya_src/include/libmqtt"
+  "source/third_party/tuya/tuya_src/include/libhttp"
+  "source/third_party/tuya/tuya_src/include/libutils"
+  "source/third_party/tuya/tuya_src/tal_kv/littlefs"
+  "source/third_party/tuya/tuya_src/libcjson/cJSON"
+  "source/third_party/tuya/tuya_src/include/tal_cli"
+  "source/third_party/tuya/tuya_src/include/tal_bluetooth"
+  "source/third_party/tuya/tuya_src/include/tal_cellular"
+  "source/third_party/tuya/tuya_src/include/tal_driver"
+  "source/third_party/tuya/tuya_src/include/tal_kv"
+  "source/third_party/tuya/tuya_src/include/tal_security"
+  "source/third_party/tuya/tuya_src/common/backoffAlgorithm/source/include"
+  "source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/include"
+  "source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/include/adc"
+  "source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/include/bluetooth"
+  "source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/include/flash"
+  "source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/include/gpio"
+  "source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/include/i2c"
+  "source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/include/i2s"
+  "source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/include/network"
+  "source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/include/pwm"
+  "source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/include/rtc"
+  "source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/include/security"
+  "source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/include/spi"
+  "source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/include/system"
+  "source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/include/timer"
+  "source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/include/uart"
+  "source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/include/wifi"
+  "source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/include/init/include"
+  "source/third_party/tuya/ti_config/tkl/tuyaos/tuyaos_adapter/include/utilities/include"
+  "C:/ti/simplelink_wifi_sdk_9_21_00_15/source"
+  "C:/ti/simplelink_wifi_sdk_9_21_00_15/source/ti/drivers/net/wifi"
+  "C:/ti/simplelink_wifi_sdk_9_21_00_15/kernel/freertos/posix"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ source/third_party/tuya/CMakeFiles/tuya.dir/tuya_src/common/utilities/crc32i.c.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles\tuya.dir\tuya_src\common\utilities\crc32i.c.obj: \
+  C:\ti\simplelink_wifi_sdk_tuya\source\third_party\tuya\tuya_src\common\utilities\crc32i.c \
+  C:\ti\simplelink_wifi_sdk_tuya\source\third_party\tuya\tuya_src\common\utilities\crc32i.h
