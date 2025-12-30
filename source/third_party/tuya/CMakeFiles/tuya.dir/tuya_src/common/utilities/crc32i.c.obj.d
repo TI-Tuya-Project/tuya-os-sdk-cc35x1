8576fb63dd6c66acbcca71054b5e20955967cddd
Samuel Lazareanu <52662032+SamuraiPolix@users.noreply.github.com>
2025-12-30 13:43:40 +0200

    Removed unwanted files marked in .gitignore

--- source/third_party/tuya/CMakeFiles/tuya.dir/tuya_src/common/utilities/crc32i.c.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles\tuya.dir\tuya_src\common\utilities\crc32i.c.obj: \
-  C:\ti\simplelink_wifi_sdk_tuya\source\third_party\tuya\tuya_src\common\utilities\crc32i.c \
-  C:\ti\simplelink_wifi_sdk_tuya\source\third_party\tuya\tuya_src\common\utilities\crc32i.h
